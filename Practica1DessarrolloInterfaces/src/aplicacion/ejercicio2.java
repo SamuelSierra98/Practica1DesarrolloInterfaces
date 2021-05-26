e4b7959770f69426b816c4f5e8ca9e837f1c6221
Samu <Samu@DESKTOP-MR6OPIL>
2021-05-26 14:16:54 +0200

    Practica 1 Desarrollo de Interfaces

---
+++ Practica1DessarrolloInterfaces/src/aplicacion/ejercicio2.java
@@ -0,0 +1,59 @@
+package aplicacion;
+
+import java.util.*;
+
+
+import constructores.Factura;
+import constructores.Producto;
+import constructores.Producto.Impuesto;
+import utilidades.UtilidadesFactura;
+
+
+public class ejercicio2 {
+		
+	static Producto producto;
+	static Factura factura;
+	static Impuesto impuestos;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		List <Producto> productos = new ArrayList<>();
+		
+		
+		
+		producto = new Producto(0.75 , "Leche" , impuestos = Impuesto.REDUCIDO);
+		productos.add(producto);
+		
+		producto = new Producto(0.50 , "Pan" , impuestos = Impuesto.SUPERREDUCIDO);
+		productos.add(producto);
+		
+		producto = new Producto(8.99 , "Salmon", impuestos = Impuesto.REDUCIDO);
+		productos.add(producto);
+		
+		producto = new Producto(2.95 , "Aguacates", impuestos = Impuesto.NORMAL);
+		productos.add(producto);
+		
+		producto = new Producto(1.20 , "Friegasuelos", impuestos = Impuesto.NORMAL);
+		productos.add(producto);
+		
+		producto = new Producto(0.56 , "Cerveza", impuestos = Impuesto.NORMAL);
+		productos.add(producto);
+		
+		producto = new Producto(1.45 , "Sandia", impuestos = Impuesto.SUPERREDUCIDO);
+		productos.add(producto);
+		
+		factura = new Factura(0 , productos, new Date()); 
+		factura.visual();
+		
+		System.out.println("\nImporte sin IVA: " + UtilidadesFactura.calcularImporteFactura(factura));
+		
+		System.out.println("Calcular Factura sin IVA : " + UtilidadesFactura.calcularIVAFactura(factura));
+		
+		System.out.println("Calcular FActura con IVA : " + UtilidadesFactura.calcularIVAFacturaConIVA(factura));
+			
+	}
+}
+		
